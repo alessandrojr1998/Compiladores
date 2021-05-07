14cc7caf755d123f848254d60421e0b6a2abd49a
Alessandro Júnior <alessandrojr1998@gmail.com>
2021-05-06 23:21:36 -0300

    add: verificações dos tokens, scanner pronto

--- Lexer/Scanner.py
+++ Lexer/Scanner.py
@@ -48,33 +48,35 @@ class Scanner:
 
     def opBolleanaToken(self, char):
       # Operações Booleanas
-      if char == "=":  # Igual ou Atribuição
-          if self.lookAhead() == "=":  # == (comparação)
-              self.atual += 1
-              return "EQUAL"
+        if char == "=":  # Igual ou Atribuição
+            if self.lookAhead() == "=":  # == (comparação)
+                self.atual += 1
+                return "EQUAL"
 
-          else:  # = (atribuição)
-              return "ATB"
+        elif char == ":":
+            if self.lookAhead() == "=": # := (atribuição)
+                self.atual += 1
+                return "ATB"        
 
-      elif char == "!":  # Diferente ("!=")
-          if self.lookAhead() == "=":
-              self.atual += 1
-              return "DIF"
+        elif char == "!":  # Diferente ("!=")
+            if self.lookAhead() == "=":
+                self.atual += 1
+                return "DIF"
 
-      elif char == "<":  # Menor ou igual, menor
-          if self.lookAhead() == "=":  # ("<= ")
-              self.atual += 1
-              return "LESSEQUAL"
+        elif char == "<":  # Menor ou igual, menor
+            if self.lookAhead() == "=":  # ("<= ")
+                self.atual += 1
+                return "LESSEQUAL"
 
-          else:  # ("<")
-              return "LESS"
+            else:  # ("<")
+                return "LESS"
 
-      elif char == ">":  # Maior ou igual, Maior
-          if self.lookAhead() == "=":  # (">=")
-              self.atual += 1
-              return "GREATEREQUAL"
-          else:  # (">")
-              return "GREATER"
+        elif char == ">":  # Maior ou igual, Maior
+            if self.lookAhead() == "=":  # (">=")
+                self.atual += 1
+                return "GREATEREQUAL"
+            else:  # (">")
+                return "GREATER"
 
     # Procura tokens até chegar no Fim
     def scanTokens(self):
@@ -105,7 +107,7 @@ class Scanner:
                     )
                 )
 
-            elif char == "=" or char == "!" or char == "<" or char == ">":
+            elif char == ":" or char == "=" or char == "!" or char == "<" or char == ">":
                 self.tokens.append(
                     Token(
                         self.opBolleanaToken(char),
@@ -154,75 +156,64 @@ class Scanner:
     def scanReserved(self):
         for i in self.tokens:
             if i.tipo == "ID":
-                # Inicio do programa
-                if i.lexema == "program":
-                  i.tipo = "PROGRAM"
+                if i.lexema == "main":
+                  i.tipo = "MAIN"
 
-                # Fim do programa
                 elif i.lexema == "end":
                   i.tipo = "END"
 
-                # Identificador de função
+                # função
                 elif i.lexema == "func":
                   i.tipo = "FUNC"
 
-                # Identificador de procedimento
+                # procedimento
                 elif i.lexema == "proc":
                   i.tipo = "PROC"
 
-                # Identificador de chamada para func
+                # chamada de func
                 elif i.lexema == "funccall":
                   i.tipo = "FUNCCALL"
 
-                # Identificador de chamada para proc
+                # chamada de proc
                 elif i.lexema == "proccall":
                   i.tipo = "PROCCALL"
 
-                # Identificador de inteiros
+                # inteiros
                 elif i.lexema == "int":
                   i.tipo = "INT"
 
-                # Tipo Booleano
-                elif i.lexema == "logic":
+                # Booleano
+                elif i.lexema == "boolean":
+                  i.tipo = "BOOLEAN"
+
+                elif i.lexema == "true":
                   i.tipo = "LOGIC"
 
-                # Booleano Verdadeiro
-                elif i.lexema == "True":
-                  i.tipo = "BOOLEAN"
+                elif i.lexema == "false":
+                  i.tipo = "LOGIC"
 
-                # Booleano Falso
-                elif i.lexema == "False":
-                  i.tipo = "BOOLEAN"
-
-                # Retorno da função
                 elif i.lexema == "return":
                   i.tipo = "RETURN"
 
-                # Condicional IF
                 elif i.lexema == "if":
                     i.tipo = "IF"
 
-                # Condicional ELSE
                 elif i.lexema == "else":
                     i.tipo = "ELSE"
                
-                # Condicional WHILE
                 elif i.lexema == "while":
                     i.tipo = "WHILE"              
 
-                # Escrita na tela
+                # Print
                 elif i.lexema == "abdullah":
-                    i.tipo = "ABDULLAH"
+                    i.tipo = "PRINT"
 
-                # Incondicional BREAK
                 elif i.lexema == "break":
                     i.tipo = "BREAK"
 
-                # Incondicional CONTINUE
                 elif i.lexema == "continue":
                     i.tipo = "CONTINUE"
 
-       # Verifica o simbolo a frente e se está no final do programa
     def lookAhead(self):
         if self.atual < len(self.programa):
             return self.programa[self.atual]
